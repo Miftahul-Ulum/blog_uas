dbdb9930353564f667048fd76cd8d2ff57cacd43
Mifathul-Ulum <aqilajati7@gmail.com>
2022-07-11 00:02:21 +0700

    change name

---
+++ resources/views/admin/templates/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+    <div class="nano">
+        <div class="nano-content">
+            <ul>
+                <div class="logo"><a href="index.html">
+                        <!-- <img src="images/logo.png" alt="" /> --><span>Aqila Jati furniture</span>
+                    </a></div>
+
+                <li class="label">Form</li>
+                <li><a href="{{ url('/admin') }}"><i class="ti-view-list-alt"></i> Dashboard </a></li>
+                <li><a href="{{ url('/admin/category') }}"><i class="ti-view-list-alt"></i> Category </a></li>
+                <li><a href="{{ url('/admin/post') }}"><i class="ti-view-list-alt"></i> Post </a></li>
+                <li class="label">Extra</li>
+
+                <li><a><i class="ti-close"></i> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /# sidebar -->
